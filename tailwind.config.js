0fcc3a13eb8902e7867dac1eed8bb6bea86cb93b
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-06-03 19:18:18 -0300

    utilerias

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      gray: colors.coolGray,
-      blue: colors.lightBlue,
-      red: colors.rose,
-      pink: colors.fuchsia,
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {},
   },
   variants: {
+    scale: ['hover'],
+    rotate:['responsive'],
     extend: {},
   },
   plugins: [],
